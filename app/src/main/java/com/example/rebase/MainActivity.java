17c583493053ccff5cb1b531afa2a75be23f5b60
cpalazis <cpalazis@yahoo.com>
2022-11-20 21:24:08 +0200

    Future  branch 2

--- app/src/main/java/com/example/rebase/MainActivity.java
+++ app/src/main/java/com/example/rebase/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("Future branch 2");
+
         System.out.println("New commit to the main");
     }
 }
